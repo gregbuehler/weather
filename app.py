4cfea5692c6dfe9059ebe6492bfb4706c7c3d609
Greg Buehler <gregb@surveymonkey.com>
2019-09-11 17:00:39 -0700

    skeleton

---
+++ app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from opentelemetry import trace
+from opentelemetry import context
+from opentelemetry.sdk.trace import Tracer
+from opentelemetry.ext.wsgi import OpenTelemetryMiddleware
+
+from flask import Flask, render_template, request
+from util import lookup
+
+app = Flask(__name__)
+app.wsgi_app = OpenTelemetryMiddleware(app.wsgi_app)
+
+@app.route('/')
+def index():
+    return render_template('index.html.j2')
+
+
+@app.route('/weather', methods=["GET", "POST"])
+@app.route('/weather/<query>', methods=["GET", "POST"])
+def weather(query=None):
+    if request.method == "POST":
+        query = request.form['query']
+    elif query == None and request.args.get('query'):
+        query = request.args.get('query')
+
+    location, weather = lookup(query)
+    return render_template('weather.html.j2', location=location, weather=weather)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
---
+++ util.py
@@ -0,0 +1,45 @@
+import os
+import requests
+from opencage.geocoder import OpenCageGeocode
+
+
+def get_location(query):
+    try:
+        key = os.environ.get('OPENCAGE_KEY')
+        geocoder = OpenCageGeocode(key)
+        results = geocoder.geocode(query)
+    except Exception as ex:
+        raise ex
+
+    return results[0]
+
+
+def get_weather(lat, lng):
+    key = os.environ.get('DARKSKY_KEY')
+    url = f"https://api.darksky.net/forecast/{key}/{lat},{lng}"
+
+    try:
+        r = requests.get(url)
+        if r.status_code != 200:
+            print(r)
+            raise Exception('Weather provider error')
+
+        weather = r.json()
+        print(weather)
+    except Exception as ex:
+        raise ex
+
+    return weather
+
+
+def lookup(query):
+    try:
+        location = get_location(query)
+        lat = location['geometry']['lat']
+        lng = location['geometry']['lng']
+
+        weather = get_weather(lat, lng)
+
+        return location, weather
+    except Exception as ex:
+        raise ex
